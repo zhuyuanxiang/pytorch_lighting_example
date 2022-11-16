9dd22c361149f251728a7ec8fa128e642199959f
zYx.Tom <ygpfr74@hotmail.com>
2022-11-16 14:00:46 +0800

    Update hydra_inherit.py
    移动其他功能代码到 hydra_bugs.py 中

--- project/examples/hydra/hydra_inherit.py
+++ project/examples/hydra/hydra_inherit.py
@@ -25,22 +25,20 @@ class DBConfig:
     host: str = "localhost"
     port: int = MISSING
     driver: str = MISSING
+    user: str = MISSING
+    password: str = MISSING
 
 
 @dataclass
 class MySQLConfig(DBConfig):
     driver: str = "mysql"
     port: int = 3306
-    user: str = MISSING
-    password: str = MISSING
 
 
 @dataclass
 class PostGreSQLConfig(DBConfig):
     driver: str = "postgresql"
-    user: str = MISSING
     port: int = 5432
-    password: str = MISSING
     timeout: int = 10
 
 
@@ -52,27 +50,15 @@ class Config:
     debug: bool = False
 
 
-@dataclass
-class AConfig(Config):
-    db = PostGreSQLConfig()
-    tmp_value = 1
-
-
-@dataclass
-class BConfig:  # 继承不能在最终类上使用，否则无法初始化。
-    a_config: Config = AConfig()
-
-
 cs = ConfigStore.instance()
-cs.store(name="base_config", node=Config)
-# cs.store(name="config_inherit", node=AConfig)
-cs.store(name="config_inherit", node=BConfig)
 cs.store(group="db", name="base_mysql", node=MySQLConfig)
 cs.store(group="db", name="base_postgresql", node=PostGreSQLConfig)
+cs.store(name="base_config", node=Config)
 
 
 @hydra.main(version_base=None, config_path='conf', config_name="config")
 def my_app(cfg: Config) -> None:
+    # 可以在命令行配置参数，可以在 config.yaml 配置参数
     print(OmegaConf.to_yaml(cfg))
 
 
